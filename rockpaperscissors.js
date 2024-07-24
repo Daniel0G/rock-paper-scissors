9c8cc8eccf3abb0116157b07678cf1b6354b2942
Daniel0G <114837295+Daniel0G@users.noreply.github.com>
2024-07-24 10:28:30 +0100

    Added the function for human choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,16 @@
-//
+// write the logic to create the computer choice
+//create a function called getComputerChoice.
+//uisng Math.random getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+
+function getHumanChoice() {
+  const playerChoice = prompt("Pick a move!");
+  return playerChoice;
+}
+console.log(getHumanChoice());
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+console.log(getComputerChoice());
+
